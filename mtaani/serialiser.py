aca6cf068f7d226ddc4a4245c0b2f43ff2c38750
xystus <xystusngigi@gmail.com>
2019-09-17 15:10:16 +0300

    changes in project folder

--- mtaani/serialiser.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Business
-
-class BusinessSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Business
-        fields = ('name', 'description', 'user','category')
--- mtaani/views.py
+++ mtaani/views.py
@@ -97,20 +97,20 @@ def businesses(request):
 
 
 @login_required
-    def post(request, id):
-        post = Post.objects.get(id=id)
-        comments = Comment.objects.filter(post=post)
-        if request.method == 'POST':
-            form = CommentForm(request.POST)
-            if form.is_valid():
-                comment = form.save(commit=False)
-                comment.user = request.user
-                comment.post = post
-                comment.save()
-            return redirect('post', id=post.id)
-        else:
-            form = CommentForm()
-        return render(request, 'post.html', {"post": post, "comments": comments, "form": form})
+def post(request, id):
+    post = Post.objects.get(id=id)
+    comments = Comment.objects.filter(post=post)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.post = post
+            comment.save()
+        return redirect('post', id=post.id)
+    else:
+        form = CommentForm()
+    return render(request, 'post.html', {"post": post, "comments": comments, "form": form})
 
 
 class BusinessList(APIView):
@@ -121,12 +121,12 @@ class BusinessList(APIView):
 
 
 @login_required
-    def search(request):
-        current_user = request.user
-        if 'search' in request.GET and request.GET["search"]:
-            search_term = request.GET.get("search")
-            businesses = Business.objects.filter(name__icontains=search_term)
-            return render(request, 'search.html', {'businesses': businesses})
-        else:
-            message = "You haven't searched for any term"
-            return render(request, 'search.html', {"message": message})
+def search(request):
+    current_user = request.user
+    if 'search' in request.GET and request.GET["search"]:
+        search_term = request.GET.get("search")
+        businesses = Business.objects.filter(name__icontains=search_term)
+        return render(request, 'search.html', {'businesses': businesses})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
