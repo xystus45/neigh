f25c526243d30a75c5eba2e7370903c3ca9ce36a
xystus <xystusngigi@gmail.com>
2019-09-18 17:06:52 +0300

    configure procfile

--- hood/wsgi.py
+++ hood/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+import django_heroku
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hood.settings')
 
 application = get_wsgi_application()
